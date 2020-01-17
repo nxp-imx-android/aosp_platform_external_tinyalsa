a972440d911cca0559851bbc54bbda94bf4047a9
Akhil Karuturi <akarutur@codeaurora.org>
2020-01-17 13:53:14 -0800

    tinyalsa: Fix error handling in plugins
    Add error handling and cleanup in plugins
    
    Bug: 166482201
    Test: audio smoke tests
    
    CRs-Fixed: 2608556
    Change-Id: I79f480bf8ae6a98150de2846380ca8857c72084e
    Signed-off-by: Akhil Karuturi <akarutur@codeaurora.org>
    (cherry picked from commit 42805c33885298d249657e0fcc375d18541e85f5)

--- pcm_plugin.c
+++ pcm_plugin.c
@@ -1,6 +1,6 @@
 /* pcm_plugin.c
 **
-** Copyright (c) 2019, The Linux Foundation. All rights reserved.
+** Copyright (c) 2019-2020, The Linux Foundation. All rights reserved.
 **
 ** Redistribution and use in source and binary forms, with or without
 ** modification, are permitted provided that the following conditions are
@@ -652,10 +652,9 @@ static int pcm_plug_open(unsigned int card, unsigned int device,
                   unsigned int flags, void **data, void *pcm_node)
 {
     struct pcm_plug_data *plug_data;
-    const char *err = NULL;
     void *dl_hdl;
     int rc = 0;
-    char *so_name, token[80], *name, *open_fn;
+    char *so_name, token[80], *name, *open_fn, *token_saveptr;
 
     plug_data = calloc(1, sizeof(*plug_data));
     if (!plug_data) {
@@ -679,8 +678,12 @@ static int pcm_plug_open(unsigned int card, unsigned int device,
     dlerror();
 
     sscanf(so_name, "lib%s", token);
-    name = strtok(token, ".");
-
+    token_saveptr = token;
+    name = strtok_r(token, ".", &token_saveptr);
+    if (!name) {
+        fprintf(stderr, "%s: invalid library name\n", __func__);
+        goto err_open_fn;
+    }
     open_fn = calloc(1, strlen(name) + strlen("_open") + 1);
     if (!open_fn) {
         rc = -ENOMEM;
@@ -692,11 +695,9 @@ static int pcm_plug_open(unsigned int card, unsigned int device,
 
     printf("%s - %s\n", __func__, open_fn);
     plug_data->plugin_open_fn = dlsym(dl_hdl, open_fn);
-    err = dlerror();
-
-    if (err) {
+    if (!plug_data->plugin_open_fn) {
         fprintf(stderr, "%s: dlsym to open fn failed, err = '%s'\n",
-                __func__, err);
+                __func__, dlerror());
         goto err_dlsym;
     }
 
@@ -720,6 +721,7 @@ static int pcm_plug_open(unsigned int card, unsigned int device,
 
     plug_data->plugin->state = PCM_PLUG_STATE_OPEN;
 
+    free(open_fn);
     return 0;
 
 err_dlsym:
